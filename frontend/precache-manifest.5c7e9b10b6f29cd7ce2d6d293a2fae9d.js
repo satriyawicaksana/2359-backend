d75b3ce96e44deab903eba588c4c32bb7dbc2b8f
satriyawicaksana <satriyawicaksana08@gmail.com>
2020-01-02 19:08:03 +0700

    uploaded frontend

---
+++ frontend/precache-manifest.5c7e9b10b6f29cd7ce2d6d293a2fae9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b41240cee22d3c039883b8c4f5e71b26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc57ececae012b7db482",
+    "url": "/static/css/main.ea394034.chunk.css"
+  },
+  {
+    "revision": "4c9f90a129514446141d",
+    "url": "/static/js/2.faee6584.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.faee6584.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc57ececae012b7db482",
+    "url": "/static/js/main.fac777ff.chunk.js"
+  },
+  {
+    "revision": "5d24dcc79f0cc4c485ce",
+    "url": "/static/js/runtime-main.8155e9a7.js"
+  },
+  {
+    "revision": "ce9d2539530c023b7cdeafc7deaf80a6",
+    "url": "/static/media/logo64.ce9d2539.svg"
+  }
+]);
